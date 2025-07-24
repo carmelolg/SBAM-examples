903dbf50022d1173ab9f49d0e562d257a38f8fa8
carmelolg <carmelolagamba@hotmail.it>
2025-07-24 23:09:26 +0200

    spring boot version updated

--- src/main/java/it/carmelolagamba/sbam/integration/http/UserRetriever.java
+++ src/main/java/it/carmelolagamba/sbam/integration/http/UserRetriever.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-import java.util.Map;
 
 @Component
 @Slf4j
